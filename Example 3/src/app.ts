cddc314fda3b5af83fb4cee9d8f42924695474ae
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-03-31 18:22:13 +0200

    TSConfig(Exclude, Include, Files, Options)
    TypeScript Course
    
    Compiler - TSConfig(Exclude, Include, Files, Options)

---
+++ Example 3/src/app.ts
@@ -0,0 +1,11 @@
+const button = document.getElementById('button')!;
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  button.addEventListener('click', function() {
+    console.log("Clicked!");
+  });
+});
+  
+
+
