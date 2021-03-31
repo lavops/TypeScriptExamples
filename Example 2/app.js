c263caf6c4adee63eec4337ca87990c33f644450
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-03-31 16:03:06 +0200

    Core Types (enum, any)
    TypeScript Course
    
    TypeScript Basics & Basic Types - Core Types (enum, any)

--- Example 2/app.js
+++ Example 2/app.js
@@ -1,7 +1,16 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: 'Example',
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [Role.ADMIN, Role[0]],
+    something: 'Something ' + 1
 };
 console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
--- Example 2/app.ts
+++ Example 2/app.ts
@@ -1,13 +1,17 @@
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+
 const person: {
     name: string;
     age: number;
     hobbies: string[]
     role: [number, string] // TUPLE TYPE
+    something: any
 } = {
     name: 'Example',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: [Role.ADMIN, Role[0]],
+    something: 'Something ' + 1
 };
 
 console.log(person);
