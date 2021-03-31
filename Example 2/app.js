e0aa8cf691c535a2975afa51196f034c0a2d7536
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-03-31 15:32:40 +0200

    Core Types (number, string, boolean, object, arrays, tuple, enum)
    TypeScript Course
    
    TypeScript Basics & Basic Types - Core Types (number, string, boolean, object, arrays, tuple, enum)

--- Example 2/app.js
+++ Example 2/app.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+var person = {
+    name: 'Example',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- Example 2/app.ts
+++ Example 2/app.ts
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[]
+    role: [number, string] // TUPLE TYPE
+} = {
+    name: 'Example',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};
+
+console.log(person);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+}
