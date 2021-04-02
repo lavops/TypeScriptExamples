8e98562569c36d17e59521d5b019fe69947a97d6
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-04-02 22:15:25 +0200

    Type Guards
    TypeScript Course
    
    Advanced Types - Type Guards

--- 6 - Advanced Types/dist/app.js
+++ 6 - Advanced Types/dist/app.js
@@ -4,3 +4,43 @@ const e1 = {
     privilages: ['create-server'],
     startDate: new Date()
 };
+// Guard Type
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') { // Type Guard
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(employee) {
+    console.log(employee.name);
+    if ('privilages' in employee) {
+        console.log(employee.privilages);
+    }
+    if ('startDate' in employee) {
+        console.log(employee.startDate);
+    }
+}
+printEmployeeInformation(e1);
+class Car {
+    drive() {
+        console.log('Driving a car...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+    loadCargo(amount) {
+        console.log('Loading cargo ... ' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(10);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- 6 - Advanced Types/src/app.ts
+++ 6 - Advanced Types/src/app.ts
@@ -21,3 +21,56 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+// Guard Type
+function add(a: Combinable, b: Combinable) {
+    if(typeof a === 'string' || typeof b === 'string') { // Type Guard
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin; // Moze da prihvati i ElevatedEmployee
+
+function printEmployeeInformation(employee: UnknownEmployee) { // Moze da prihvati i ElevatedEmployee
+    console.log(employee.name);
+    if('privilages' in employee) {
+        console.log(employee.privilages);
+    }
+    if('startDate' in employee) {
+        console.log(employee.startDate);
+    }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+    drive() {
+        console.log('Driving a car...');
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+
+    loadCargo(amount: number) {
+        console.log('Loading cargo ... ' + amount);
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    if(vehicle instanceof Truck) {
+        vehicle.loadCargo(10);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
