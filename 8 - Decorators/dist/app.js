fd4f2d6f3646d92290fc0c32d148af5a3dd9d808
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-04-03 21:25:06 +0200

    Property, Accessor, Method & Parameter Decorators
    TypeScript Course
    
    Decorators - Property, Accessor, Method & Parameter Decorators

--- 8 - Decorators/dist/app.js
+++ 8 - Decorators/dist/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Logger(logString) {
     console.log("LOGGER FACTORY");
     return function (target) {
@@ -36,3 +39,56 @@ Person = __decorate([
 ], Person);
 const person = new Person();
 console.log(person);
+// Property Decorator
+function Log(target, propertyName) {
+    console.log('Property decorator...');
+    console.log(target, propertyName);
+}
+// Accessor Decorator
+function Log2(target, name, descriptor) {
+    console.log('Accessor decorator...');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+// Method Decorator
+function Log3(target, name, descriptor) {
+    console.log('Method decorator...');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+// Parameter Decorator
+function Log4(target, name, position) {
+    console.log('Parameter decorator...');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    constructor(title, price) {
+        this.title = title;
+        this._price = price;
+    }
+    set price(value) {
+        if (value > 0) {
+            this._price = value;
+        }
+        else {
+            throw new Error('Invalid price - should be positive');
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
--- 8 - Decorators/src/app.ts
+++ 8 - Decorators/src/app.ts
@@ -30,3 +30,58 @@ class Person {
 
 const person = new Person();
 console.log(person);
+
+// Property Decorator
+function Log(target: any, propertyName: string) {
+    console.log('Property decorator...');
+    console.log(target, propertyName);
+}
+
+// Accessor Decorator
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log('Accessor decorator...');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+// Method Decorator
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+    console.log('Method decorator...');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+// Parameter Decorator
+function Log4(target: any, name: string | Symbol, position: number) {
+    console.log('Parameter decorator...');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    constructor(title: string, price: number) {
+        this.title = title;
+        this._price = price;
+    }
+
+    @Log2
+    set price(value: number) {
+        if(value > 0){
+            this._price = value;
+        }
+        else {
+            throw new Error('Invalid price - should be positive');
+        }
+    }
+
+    @Log3
+    getPriceWithTax(@Log4 tax: number) {
+        return this._price * (1 + tax);
+    }
+}
