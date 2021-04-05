d4e0b5215aa35348b3c7c743a78507708876209e
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-04-05 17:34:59 +0200

    Interacting with DOM Elements
    Drag & Drop Project - Interacting with DOM Elements

--- 9 - Drag & Drop Project/dist/app.js
+++ 9 - Drag & Drop Project/dist/app.js
@@ -5,7 +5,19 @@ class ProjectInput {
         this.hostElement = document.getElementById('app');
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputElement = document.querySelector('#title');
+        this.descriptionInputElement = document.querySelector('#description');
+        this.peopleInputElement = document.querySelector('#people');
+        this.configure();
         this.attach();
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
