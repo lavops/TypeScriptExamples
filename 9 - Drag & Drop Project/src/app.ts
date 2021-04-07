8818a3ff0fb33cc5115660d3becdd4e8f67eba31
Djordje Milicevic <milicevic.djordje@hotmail.com>
2021-04-07 12:17:45 +0200

    Using a Getter from ProjectItem class
    Drag & Drop Project - Using a Getter from ProjectItem class

--- 9 - Drag & Drop Project/src/app.ts
+++ 9 - Drag & Drop Project/src/app.ts
@@ -135,6 +135,15 @@ enum ProjectStatus {
     
     private project: Project;
 
+    get persons() {
+        if(this.project.people === 1) {
+            return '1 person';
+        }
+        else {
+            return `${this.project.people} persons`
+        }
+    }
+
     constructor(hostId: string, project: Project) {
         super('single-project', hostId, false, project.id);
         this.project = project;
@@ -149,7 +158,7 @@ enum ProjectStatus {
 
     renderContent(): void {
         this.element.querySelector('h2')!.textContent = this.project.title;
-        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned.';
         this.element.querySelector('p')!.textContent = this.project.description;
     }
   }
